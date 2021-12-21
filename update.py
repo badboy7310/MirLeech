1ae7da6ca817021437e7aeb88d7c0d9a7b14c0c4
Badnam Aashiq <92842718+badboy7310@users.noreply.github.com>
2021-12-21 22:44:22 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/arshsisodiya/helios-mirror-public"
+    UPSTREAM_REPO = "https://github.com/badboy7310/helios-mirror-public"
 
 if os.path.exists('.git'):
     subprocess.run(["rm", "-rf", ".git"])
